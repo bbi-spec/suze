acb8b5ea92bc893915fea83d730cdb3ea56bd5c8
bbi-spec <zebzbbi@gmail.com>
2025-12-18 20:31:54 -0600

    Update ghub.h

--- sunone_aimbot_cpp/mouse/ghub.h
+++ sunone_aimbot_cpp/mouse/ghub.h
@@ -1,7 +1,6 @@
 #ifndef GHUB_H
 #define GHUB_H
 
-#include <filesystem>
 #include <windows.h>
 
 class GhubMouse
@@ -9,21 +8,16 @@ class GhubMouse
 public:
     GhubMouse();
     ~GhubMouse();
+
+    // Core movement/click functions
     bool mouse_xy(int x, int y);
     bool mouse_down(int key = 1);
     bool mouse_up(int key = 1);
     bool mouse_close();
 
 private:
-    std::filesystem::path basedir;
-    std::filesystem::path dlldir;
-    HMODULE gm;
+    // We only need this flag to track if the socket is ready
     bool gmok;
-
-    static UINT _ghub_SendInput(UINT nInputs, LPINPUT pInputs);
-    static INPUT _ghub_Input(MOUSEINPUT mi);
-    static MOUSEINPUT _ghub_MouseInput(DWORD flags, LONG x, LONG y, DWORD data);
-    static INPUT _ghub_Mouse(DWORD flags, LONG x = 0, LONG y = 0, DWORD data = 0);
 };
 
 #endif // GHUB_H
